612f79811647c97bcb3688725ed9c9dcb99d196a
Gabriel <rdeckard.gt@gmail.com>
2020-05-06 17:53:17 +0200

    Update chess.rb
    Side note:
    There was a typo in your [Shebang](https://fr.wikipedia.org/wiki/Shebang).
    Now this script can work directly (and the ".rb" extension is optional):
    `./chess R..P. '.....' QBK.P .R... .PPB.`

--- chess.rb
+++ chess.rb
@@ -1,185 +1,215 @@
-#!usr/bin/env ruby
+#!/usr/bin/env ruby
+# frozen_string_literal: true
 
-if (ARGV.length > 1)
-    class ChessGame
-        attr_accessor :board
-        attr_accessor :posy
-        attr_accessor :posx
-        def initialize
-            @board = Array.new(ARGV.length) { Array.new(ARGV.length) }
+# LIBRARY
+class ChessGame
+  PIECE_NAMES =
+    Hash.new('empty boxes').merge!(
+      'K' => 'king',
+      'P' => 'pawn',
+      'Q' => 'queen',
+      'R' => 'rook',
+      'B' => 'bishop'
+    ).freeze
+
+  attr_reader :board
+
+  def initialize(board)
+    check_input(board)
+
+    @board = board
+
+    localize_king
+  end
+
+  def check
+    ### Check the 2 horizontal directions (left and right) for R & Q ###
+    board[0..king_posy]
+      .map { |line| line[king_posx] }
+      .reverse_each do |piece|
+        case piece
+        when 'P', 'B'
+          break
+        when 'R', 'Q'
+          return annonce_a_check_with(piece)
         end
+      end
 
-        / CREATION DE LA MAP /
-        def fill_board
-            i = 0
-            while i < ARGV.length do
-                @board[i] = ARGV[i]
-                i += 1
-            end
+    board[king_posy...board.size]
+      .map { |line| line[king_posx] }
+      .each do |piece|
+        case piece
+        when 'P', 'B'
+          break
+        when 'R', 'Q'
+          return annonce_a_check_with(piece)
         end
-       
-        / RECHERCHE POSITION DU ROI /
-        def find_king
-            y = 0
-            while y < ARGV.length do
-                x = 0
-                while x < ARGV.length do
-                    if @board[y][x] == "K"
-                        @posy = y
-                        @posx = x
-                        return
-                    end
-                    x += 1
-                end
-                y += 1
-            end
-            if !posx && !posy
-                print "You must place a king on the map", "\n"
-                exit (true)
-            end
-        end
+      end
 
-        / ALGO VERIFICATION SI ROI EN ECHEC /
-        def check
-
-            / AFFICHAGE DE LA MAP /
-            def print_map
-                i = 0
-                print "Map:", "\n"
-                while i < ARGV.length do
-                    print @board[i], "\n"
-                    i += 1
-                end
-            end
-
-            y = posy
-            x = posx
-            while y >= 0 do
-                if @board[y][x] == "P" || @board[y][x] == "B"
-                    break 
-                elsif @board[y][x] == "R" || @board[y][x] == "Q"
-                    return (print "THE KING IS IN CHECK !", "\n")
-                end
-                y -= 1
-            end
-
-            y = posy
-            while y < ARGV.length do
-                if @board[y][x] == "P" || @board[y][x] == "B"
-                    break
-                elsif @board[y][x] == "R" || @board[y][x] == "Q"
-                    return (print "THE KING IS IN CHECK !", "\n")
-                end
-                y += 1
-            end
-
-            y = posy
-            while x >= 0 do
-                if @board[y][x] == "P" || @board[y][x] == "B"
-                    break
-                elsif @board[y][x] == "R" || @board[y][x] == "Q"
-                    return (print "THE KING IS IN CHECK !", "\n")
-                end
-                x -= 1
-            end
-
-            x = posx
-            while x < ARGV.length do
-                if @board[y][x] == "P" || @board[y][x] == "B"
-                    break
-                elsif @board[y][x] == "R" || @board[y][x] == "Q"
-                    return (print "THE KING IS IN CHECK !", "\n")
-                end
-                x += 1
-            end
-
-            y = posy
-            x = posx
-            while y >= 0 && x >= 0 do
-                if @board[y][x] == "P" || @board[y][x] == "R"
-                    break
-                elsif @board[y][x] == "B" || @board[y][x] == "Q"
-                    return (print "THE KING IS IN CHECK !", "\n")
-                end
-                y -= 1
-                x -= 1
-            end
-
-            y = posy
-            x = posx
-            while y >= 0 && x < ARGV.length do
-                if @board[y][x] == "P" || @board[y][x] == "R"
-                    break
-                elsif @board[y][x] == "B" || @board[y][x] == "Q"
-                    return (print "THE KING IS IN CHECK !", "\n")
-                end
-                y -= 1
-                x += 1
-            end
-
-            y = posy
-            x = posx
-            while y < ARGV.length && x >= 0 do
-                if @board[y][x] == "P" || @board[y][x] == "R"
-                    break
-                elsif @board[y][x] == "B" || @board[y][x] == "Q"
-                    return (print "THE KING IS IN CHECK !", "\n")
-                end
-                y += 1
-                x -= 1
-            end
-
-            y = posy
-            x = posx
-            while y < ARGV.length && x < ARGV.length do
-                if @board[y][x] == "P" || @board[y][x] == "R"
-                    break
-                elsif @board[y][x] == "B" || @board[y][x] == "Q"
-                    return (print "THE KING IS IN CHECK !", "\n")
-                end
-                y += 1
-                x += 1
-            end
-
-            if posy + 1 < ARGV.length && posx + 1 < ARGV.length
-                if @board[posy + 1][posx + 1] == "P"
-                    return (print "THE KING IS IN CHECK !", "\n")
-                end
-            end
-            if posy + 1 < ARGV.length && posx - 1 >= 0
-                if @board[posy + 1][posx - 1] == "P"
-                    return (print "THE KING IS IN CHECK !", "\n")
-                end
-            end
-            print "The king is not in check.", "\n"
-        end
+    ### Check the 2 vertical directions (up and down) for R & Q ###
+    board[king_posy][0..king_posx].reverse_each do |piece|
+      case piece
+      when 'P', 'B'
+        break
+      when 'R', 'Q'
+        return annonce_a_check_with(piece)
+      end
     end
-    chess = ChessGame.new
-    chess.fill_board
-    chess.find_king
-    chess.check
-    chess.print_map
 
-/ NOMBRE D'ARGUMENTS NON VALIDE /
-else
-    print "To few arguments","\n"
-    / CREATION MAP ET REGLES /
-    def how_to_create_map
-        puts "Rules: Place a king in a square map, and the desired number of pieces, and this script will tell you if the king is in check."
-        puts "To create a map:
+    board[king_posy][king_posx...board.size].each do |piece|
+      case piece
+      when 'P', 'B'
+        break
+      when 'R', 'Q'
+        return annonce_a_check_with(piece)
+      end
+    end
+
+    ### Check the 4 diagonal directions for B & Q ###
+    [king_posy, king_posx].then do |y, x|
+      while inside_board?(y, x)
+        piece = board[y][x]
+
+        case piece
+        when 'P', 'R'
+          break
+        when 'B', 'Q'
+          return annonce_a_check_with(piece)
+        end
+
+        y -= 1
+        x -= 1
+      end
+    end
+
+    [king_posy, king_posx].then do |y, x|
+      while inside_board?(y, x)
+        piece = board[y][x]
+
+        case piece
+        when 'P', 'R'
+          break
+        when 'B', 'Q'
+          return annonce_a_check_with(piece)
+        end
+
+        y -= 1
+        x += 1
+      end
+    end
+
+    [king_posy, king_posx].then do |y, x|
+      while inside_board?(y, x)
+        piece = board[y][x]
+
+        case piece
+        when 'P', 'R'
+          break
+        when 'B', 'Q'
+          return annonce_a_check_with(piece)
+        end
+
+        y += 1
+        x -= 1
+      end
+    end
+
+    [king_posy, king_posx].then do |y, x|
+      while inside_board?(y, x)
+        piece = board[y][x]
+
+        case piece
+        when 'P', 'R'
+          break
+        when 'B', 'Q'
+          return annonce_a_check_with(piece)
+        end
+
+        y += 1
+        x += 1
+      end
+      "jij"
+    end # => [king_posy, king_posx]
+
+    ### Check for P ###
+    if inside_board?(king_posy + 1, king_posx + 1) &&
+       board[king_posy + 1][king_posx + 1] == 'P'
+      return annonce_a_check_with('P')
+    end
+    if inside_board?(king_posy + 1, king_posx - 1) &&
+       @board[king_posy + 1][king_posx - 1] == 'P'
+      return annonce_a_check_with('P')
+    end
+
+    'The king is not in check.'
+  end
+
+  private
+
+  attr_reader :king_posy, :king_posx
+
+  def localize_king
+    board.each_with_index do |line, y|
+      line.each_with_index do |piece, x|
+        next unless piece == 'K'
+
+        @king_posy = y
+        @king_posx = x
+        break
+      end
+    end
+
+    return unless king_posx.nil? || king_posy.nil?
+
+    puts 'You must place a king on the map.'
+    exit
+  end
+
+  def annonce_a_check_with(piece)
+    "THE #{PIECE_NAMES[piece].upcase} HAS THE KING IS IN CHECK!"
+  end
+
+  def inside_board?(y, x)
+    y >= 0 && x >= 0 && y < board.size && x < board.size
+  end
+
+  def check_input(board)
+    return if board.size > 1
+
+    puts <<~TEXT
+      To few arguments
+      Rules:
+        - Place a king in a square map, and the desired number of pieces, and this script will tell you if the king is in check.
+
+      To create a map:
         - Each argument is a line, the map must be a square, can have only one king and there can be the following pieces:
-        - 'K' for a king
-        - 'P' for pawn
-        - 'Q' for a queen
-        - 'R' for rook
-        - 'B' for bishop
-        - '.' for the empty boxes
-        Example, for a 5x5 map (five arguments of five characters each):
-        First argument:  R..P.
-        Second argument: .....
-        Third argument:  QBK.P
-        Fourth argument: .R...
-        Fifth argument:  .PPB."
-    end
-    how_to_create_map
+        #{
+          %w[K P Q R B .].map do |piece|
+            piece_name = ChessGame::PIECE_NAMES[piece]
+
+            "- '#{piece}'" \
+            " for a#{'n' if piece_name.downcase.start_with?('a', 'e', 'i', 'o', 'u', 'y')}" \
+            " #{piece_name.split.map(&:capitalize).join(' ')}"
+          end.join("\n  ")
+        }
+
+      Example, for a 5x5 map (five arguments of five characters each), 5 arguments:
+        R..P.
+        .....
+        QBK.P
+        .R...
+        .PPB.
+      i.e. in CLI: `./chess R..P. '.....' QBK.P .R... .PPB.`
+    TEXT
+    exit
+  end
 end
+
+puts self.class
+
+# # PROGRAM
+# chess = ChessGame.new(ARGV.map(&:chars))
+
+# puts chess.check
+# puts 'Board:', chess.board.map(&:join)
+
